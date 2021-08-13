9922563e844f02dcf041196d4e3c6e4469241dd9
EliasPiolatto <eliaspiolatto77@hotmail.com>
2021-08-13 15:07:29 -0300

    Archivo guardado, HOMEWORK1.js 02-JS-I

---
+++ 02-JS-I/homework/HOMEWORK1.js
@@ -0,0 +1,15 @@
+Variables: una variable es una forma de almacenar algo para usar mas tarde, ésta se puede configurar y restablecer.
+Para crear una se utiliza la abreviacion "var" seguido de un espacio "nombre" espacio "=" espacio "valor".
+
+String: son bloques de TextDecoder, siempre van entre comillas.
+
+Valores Booleanos: provienen de la logica Boole. Es un concepto de codigo binario, significa que tiene dos opciones, activar o desactivar, 0 o 1, verdadero o falso.
+En JS se usa para significar verdadero o falso. 
+
+Funciones: son unas de las partes mas importantes del lenguaje de programacion. Son objetos particulares y obtienen las mismas caracteristicas de los objetos comunes.
+
+Argumentos: si le damos a una variable entre parentesis una razon u objeto dentro de nuestra funcion, a eso se le llama "Argumento".
+
+Return: se utiliza para que la funcion que estamos ejecutando se frene y nos dé una "devolucion".
+
+Declaraciones if: es una forma de que nuestra funcion verifique si algo es true o false. Usaremos la palabra clave if, junto con operadores de comparacion. 
