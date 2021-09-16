bfe327455537bccc9944d4eda501a49db67390b0
EliasPiolatto <eliaspiolatto77@hotmail.com>
2021-09-16 14:28:36 -0300

    Agrego Tarea Teorica 05-JS-IV

---
+++ 05-JS-IV/homework/TareaTeorica.js
@@ -0,0 +1,20 @@
+Objetos: los objetos contienen mucha información sobre una sola cosa. Los objetos se instancian usando llaves ({}).
+
+
+
+Propiedades: Los objetos usan un concepto llamado pares de clave:valor. La clave (key) es el identificador y el valor (value) es el valor que queremos guardar en esa clave. La sintaxis es "clave: valor".
+Los valores pueden ser cualquier tipo de dato de Javascript.
+
+
+
+Métodos: En los objetos, los valores se pueden establecer en funciones. Las funciones guardadas en un objeto se denominan métodos.
+
+
+
+Bucle for…in: entre paréntesis declararemos una variable, la palabra clave in y el nombre del objeto. Esto recorrerá cada clave del objeto y finalizará cuando se hayan iterado todas las claves. Podemos usar esta clave, y la notación de corchetes, en nuestro bucle for para acceder al valor asociado con esa clave.
+
+
+
+Notación de puntos vs notación de corchetes: 
+Con la notación de puntos podemos llamar al nombre del objeto, un punto y el nombre de la clave.
+La notación de corchetes es como llamar a un elemento en una matriz, aunque con corchetes debemos usar una cadena o número, o una variable que apunte a una cadena o número. Se puede llamar a cada clave envolviéndola con comillas.
