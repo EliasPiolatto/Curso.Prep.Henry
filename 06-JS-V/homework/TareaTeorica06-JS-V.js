53df7a89292d6347ca1a4f134dfda1256127d74e
EliasPiolatto <eliaspiolatto77@hotmail.com>
2021-11-09 20:18:13 -0300

    Agrego teorico 06-JS-V

---
+++ 06-JS-V/homework/TareaTeorica06-JS-V.js
@@ -0,0 +1,3 @@
+Prototype: es una forma de crear una funcion para una ChannelSplitterNode, y luego poder acceder a ella. 
+
+Constructor: es un objeto que contiene propiedades a las cuales se puede acceder a ellas o heredarlas a otro constructor o clase . 
