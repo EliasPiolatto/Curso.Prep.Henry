420da535b248871da9e4a9184475c77a33d48ba7
EliasPiolatto <eliaspiolatto77@hotmail.com>
2021-11-23 17:12:26 -0300

    Agrego Teorico 07-JS-VI

---
+++ 07-JS-VI/homework/Teorico-07-JS-VI.js
@@ -0,0 +1,7 @@
+Callback: sirve para pasar una funcion como argumento a otra funcion. 
+
+.forEach: toma un callback como su unico argumento, e itera sobre cada elemento de la matriz y llama al CB en él. 
+
+.reduce: ejecuta un bucle en la matriz con la intencion de reducir cada elemento en un elemento que se devuelve. 
+
+.map: se usa para cambiar cada elemento de una matriz de la misma manera. 
